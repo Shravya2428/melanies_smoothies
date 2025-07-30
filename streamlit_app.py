68b8629f516b4acb463196c47a57af4b7e029011
Shravya2428 <shravyashetty3800@gmail.com>
2025-07-30 14:12:29 -0400

    Update streamlit_app.py
    Adding loc and iloc pandas functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert the Snowpark dataframe to Pandas Dataframe so that we can use the LOC function
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list = st.multiselect("Choose upto 5 ingredients :",my_dataframe, max_selections = 5)
 
@@ -34,6 +34,9 @@ if ingredients_list:
       ingredients_string += fruit_chosen + ' '
       smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
       sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+
+      search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+      st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
